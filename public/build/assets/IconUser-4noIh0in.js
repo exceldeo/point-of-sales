0f371a318089b6edd93fd90e23b55e0a02ce740b
excel.cornelius <excel.cornelius@gmail.com>
2026-03-02 18:29:20 +0800

    Refactor code structure and remove redundant code blocks for improved readability and maintainability

---
+++ public/build/assets/IconUser-4noIh0in.js
@@ -0,0 +1,11 @@
+import{c as e}from"./createReactComponent-DwqUbR-v.js";/**
+ * @license @tabler/icons-react v3.5.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var c=e("outline","check","IconCheck",[["path",{d:"M5 12l5 5l10 -10",key:"svg-0"}]]);/**
+ * @license @tabler/icons-react v3.5.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var o=e("outline","user","IconUser",[["path",{d:"M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0",key:"svg-0"}],["path",{d:"M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2",key:"svg-1"}]]);export{c as I,o as a};
